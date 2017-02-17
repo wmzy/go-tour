0073b52f710de8177fd008a9561e56a726099221
zhuletian <zhuletian@xiaomi.com>
2017-02-17 16:36:53 +0800

    exercise errors

---
+++ exercise-errors.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	return 0, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
